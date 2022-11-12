bccf159d561be25e6c23ffa5ce363fe806004809
Ettore Rollo <ettorerollo@hotmail.com>
2022-11-11 22:09:10 -0300

    Refatorado métodos para testes com JUnit

--- src/test/java/Conta.java
+++ src/test/java/Conta.java
@@ -1,6 +1,7 @@
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 public class Conta {
 	
@@ -20,7 +21,7 @@ public class Conta {
 	 * Declaração da variável que irá indicar se o cliente é
 	 * do tipo especial
 	 */
-	boolean clienteEspecial = true;
+	boolean clienteEspecial;
 	
 	public int getSaldo() {
 		return saldo;
@@ -53,22 +54,21 @@ public class Conta {
 	 * @param int1 é o primeiro parâmetro de teste, referente ao saldo atual do cliente especial
 	 * Nesse método, esperasse que o cliente seja do tipo especial e que o saldo seja um inteiro
 	 */
-	@Given("Um cliente especial com saldo atual de {int} reais")
-	public void um_cliente_especial_com_saldo_atual_de_reais(Integer int1) {
-		this.saldo = int1;
-		if(this.clienteEspecial != true || !(int1 instanceof Integer))
-			throw new io.cucumber.java.PendingException();
+	@Given("^Um cliente especial com saldo atual de -(\\d+) reais$")
+	public void um_cliente_especial_com_saldo_atual_de_reais(int int1) throws Throwable {
+		setSaldo(int1 * -1);
+		setClienteEspecial(true);
+		if(this.clienteEspecial != true)
+			throw new PendingException();
 	}
 
 	/*
 	 * @param int2 é o segundo parâmetro de teste, referente ao valor de saque efetuado pelo cliente especial
 	 * Nesse método, esperasse que o valor recebido seja um inteiro
 	 */	
-	@When("for solicitado um saque no valor de {int} reais")
-	public void for_solicitado_um_saque_no_valor_de_reais(Integer int2) {
-		this.saque = int2;
-		if(!(int2 instanceof Integer))
-			throw new io.cucumber.java.PendingException();
+	@When("^for solicitado um saque no valor de (\\d+) reais$")
+	public void for_solicitado_um_saque_no_valor_de_reais(int int2) throws Throwable {
+		setSaque(int2);
 	}
 
 	/*
@@ -77,15 +77,11 @@ public class Conta {
 	 * Nesse método, esperasse que o valor de saldo seja subtraido do saque e que o resultado seja igual ao
 	 * int3, e que o int3 seja um número inteiro
 	 */
-	@Then("deve efetuar o saque e atualizar o saldo da conta para {int} reais")
-	public void deve_efetuar_o_saque_e_atualizar_o_saldo_da_conta_para_reais(Integer int3) {
-		if(int3 instanceof Integer)
-		{
-			setSaldo(getSaldo() - getSaque());
-			if(getSaldo() != int3)
-				throw new io.cucumber.java.PendingException();
-		} else
-			throw new io.cucumber.java.PendingException();
+	@Then("^deve efetuar o saque e atualizar o saldo da conta para -(\\d+) reais$")
+	public void deve_efetuar_o_saque_e_atualizar_o_saldo_da_conta_para_reais(int int3) throws Throwable {
+		setSaldo(getSaldo() - getSaque());
+		if(getSaldo() != (int3 * -1))
+			throw new PendingException();
 	}
 
 	/* 
@@ -94,34 +90,32 @@ public class Conta {
 	 * @param int4 é o quarto parâmetro de teste, referente ao saldo atual do cliente comum
 	 * Nesse método, esperasse que o valor seja um inteiro e que o cliente seja do tipo comum
 	 */
-	@Given("Um cliente comum com saldo atual de {int} reais")
-	public void um_cliente_comum_com_saldo_atual_de_reais(Integer int4) {
-		this.saldo = int4;
-		if(this.clienteEspecial == true || !(int4 instanceof Integer))
-			throw new io.cucumber.java.PendingException();
+	@Given("^Um cliente comum com saldo atual de (\\d+) reais$")
+	public void um_cliente_comum_com_saldo_atual_de_reais(int int4) throws Throwable {
+		setSaldo(int4);
+		setClienteEspecial(false);
+		if(isClienteEspecial())
+			throw new PendingException();
 	}
 
 	/*
 	 * @param int5 é o quinto parâmetro de teste, referente ao valor de saque efetuado pelo cliente comum
 	 * Nesse método, esperasse que o valor recebido seja um inteiro
 	 */
-	@When("solicitar um saque de {int} reais")
-	public void solicitar_um_saque_de_reais(Integer int5) {
-		this.saque = int5;
-		if(!(int5 instanceof Integer))
-			throw new io.cucumber.java.PendingException();
+	@When("^solicitar um saque de (\\d+) reais$")
+	public void solicitar_um_saque_de_reais(int int5) throws Throwable {
+		setSaque(int5);
 	}
 
 	/*
 	 * Nesse método, esperasse que o cliente seja do tipo normal, se o saldo for menor que
 	 * o valor de saque, deve ser retornado uma mensagem de "Saldo Insuficiente".
 	 */
-	
-	@Then("Não deve efetuar o saque e deve retornar a mensagem Saldo Insuficiente")
-	public void não_deve_efetuar_o_saque_e_deve_retornar_a_mensagem_saldo_insuficiente() {
-		if(!(getSaldo() > 0) && (getSaque() < getSaldo()))
-			throw new io.cucumber.java.PendingException();
-		if(getSaque() >= getSaldo())
+	@Then("^Não deve efetuar o saque e deve retornar a mensagem Saldo Insuficiente$")
+	public void não_deve_efetuar_o_saque_e_deve_retornar_a_mensagem_Saldo_Insuficiente() throws Throwable {
+		if(getSaldo() < 0)
+			throw new PendingException();
+		if(getSaque() > getSaldo())
 			System.out.println("Saldo Insuficiente");
 	}
 }
